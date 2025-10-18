f64832d675958e436ae5d7c8f389760a22f87608
Son Nguyen <SonNTHE181860@fpt.edu.vn>
2025-10-18 10:40:43 +0700

    add DBContext

--- PRM392_API/ProgramConfig/Son.cs
+++ PRM392_API/ProgramConfig/Son.cs
@@ -1,12 +1,14 @@
-﻿namespace PRM392_API.ProgramConfig
+﻿using PRM392_API.Repositories.Implementation;
+using PRM392_API.Repositories.Interface;
+
+namespace PRM392_API.ProgramConfig
 {
 	public static class Son
 	{
 		public static IServiceCollection AddMyServices2(this IServiceCollection services)
 		{
-			//Configure services in program.cs here
-
-			return services;
+            services.AddScoped<IGroupTaskRepository,GroupTaskRepository>();
+            return services;
 		}
 	}
 }
