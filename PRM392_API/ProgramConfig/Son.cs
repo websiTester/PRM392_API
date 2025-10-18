7098d8f0dd8d5858d580f7743952d01aa724e18e
Son Nguyen <SonNTHE181860@fpt.edu.vn>
2025-10-18 16:39:02 +0700

    reomve https

--- PRM392_API/ProgramConfig/Son.cs
+++ PRM392_API/ProgramConfig/Son.cs
@@ -1,5 +1,7 @@
 ﻿using PRM392_API.Repositories.Implementation;
 using PRM392_API.Repositories.Interface;
+using PRM392_API.Services.Implementation;
+using PRM392_API.Services.Interface;
 
 namespace PRM392_API.ProgramConfig
 {
@@ -8,6 +10,7 @@ namespace PRM392_API.ProgramConfig
 		public static IServiceCollection AddMyServices2(this IServiceCollection services)
 		{
             services.AddScoped<IGroupTaskRepository,GroupTaskRepository>();
+			services.AddScoped<IGroupTaskService,GroupTaskService>();
             return services;
 		}
 	}
