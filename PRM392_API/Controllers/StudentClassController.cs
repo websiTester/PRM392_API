abc3ab232aa41a34e38e807a273ef3c021906851
QuanNT24 <quannt2403pc@gmail.com>
2025-11-04 09:42:02 +0700

    Add Show all class and Join class for Student

---
+++ PRM392_API/Controllers/StudentClassController.cs
@@ -0,0 +1,61 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using PRM392_API.Services.Interface;
+using PRM392_API.ViewModels;
+
+namespace PRM392_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentClassController : ControllerBase
+    {
+        private readonly IStudentClassService _studentClassService;
+        //private readonly UserManager<User> _userManager;
+
+        public StudentClassController(IStudentClassService studentClassService)
+        {
+            _studentClassService = studentClassService;
+            //_userManager = userManager;
+        }
+
+        [HttpGet("GetClasses")]
+        public async Task<IActionResult> GetClasses()
+        {
+            //User user = await _userManager.GetUserAsync(User);
+
+            //if (user == null)
+            //{
+            //    return Unauthorized(new { success = false, message = "Bạn cần đăng nhập để xem lớp học." });
+            //}
+
+
+
+            int currentUserId = 107;
+
+            IEnumerable<StudentClassListDto> studentClasses = _studentClassService.GetClassesByStudentId(currentUserId);
+
+            return Ok(studentClasses);
+        }
+
+     
+        [HttpPost("Join")]
+        public async Task<IActionResult> JoinClass([FromBody] JoinClassViewModel model)
+        {
+            //User user = await _userManager.GetUserAsync(User);
+
+            //if (user == null)
+            //{
+            //    return Unauthorized(new { success = false, message = "Bạn cần đăng nhập để tham gia lớp." });
+            //}
+
+            var result = _studentClassService.JoinClass(107, model.ClassCode);
+
+            if (result.success)
+            {
+                return Ok(new { success = true, message = result.message });
+            }
+
+            return BadRequest(new { success = false, message = result.message });
+        }
+    }
+}
---
+++ PRM392_API/Repositories/Interface/IStudentClassRepository.cs
@@ -0,0 +1,11 @@
+﻿using PRM392_API.Models;
+
+namespace PRM392_API.Repositories.Interface
+{
+    public interface IStudentClassRepository
+    {
+        IEnumerable<StudentClass> GetClassesByStudentId(int studentId);
+        bool IsStudentInClass(int studentId, int classId);
+        void AddStudentToClass(StudentClass studentClass);
+    }
+}
